8039e842f1c288c2bdbc2de15a77817f30f300fc
jiyinyiyong <jiyinyiyong@gmail.com>
2018-07-08 01:35:45 +0800

    release to site

--- README.md
+++ README.md
@@ -2,35 +2,13 @@
 Sedum Slide
 ----
 
-Demo http://repo.tiye.me/sedum-slide/
+Demo http://repo.memkits.org/sedum-slide/
 
 A small slide tool to be less disturbing.
 
-### Logo
+### Workflow
 
-![](./images/sedum-320x320.png)
-![](./images/sketch.png)
-
-Imspired by: https://dribbble.com/shots/1847084-Sedum-Repestre-Angelina?list=searches&offset=0
-
-![](./images/inspiration.png)
-
-### Usage
-
-* Development
-
-```text
-npm i
-gulp html
-webpack-dev-server --hot
-```
-
-* Production
-
-```text
-gulp buld
-gulp rsync
-```
+https://github.com/mvc-works/calcit-workflow
 
 ### License
 
--- src/app/config.cljs
+++ src/app/config.cljs
@@ -9,11 +9,11 @@
     (not (contains? bundle-builds (get-env! "mode")))))
 
 (def site
-  {:storage "workflow",
+  {:storage "sedum-slide",
    :dev-ui "http://localhost:8100/main.css",
    :release-ui "http://cdn.tiye.me/favored-fonts/main.css",
-   :cdn-url "http://cdn.tiye.me/calcit-workflow/",
-   :cdn-folder "tiye.me:cdn/calcit-workflow",
-   :title "Calcit",
+   :cdn-url "http://cdn.tiye.me/sedum-slide/",
+   :cdn-folder "tiye.me:cdn/sedum-slide",
+   :title "Sedum Slide",
    :icon "http://cdn.tiye.me/logo/mvc-works.png",
-   :upload-folder "tiye.me:repo/mvc-works/calcit-workflow/"})
+   :upload-folder "tiye.me:repo/Memkits/sedum-slide/"})
